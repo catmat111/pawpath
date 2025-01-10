9f279fa9df123122b82aa57d8140c74e1728e70a
CatarinaMatos <aluno25976@ipt.pt>
2025-01-10 00:53:02 +0000

    alterações

--- Pawpath/src/components/feed_procurado/feed_procurado.jsx
+++ Pawpath/src/components/feed_procurado/feed_procurado.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './feed_procurado.css';
-import Navbar from '../navbar/navbar.jsx'; // Importando Navbar com a primeira letra maiúscula
+import Navbar from '../navbar/navbar.jsx'; 
 import { useLocation } from 'react-router-dom';
 
 export default function FeedProcurado() {
-    const location = useLocation(); // Obtém o estado da navegação
-    const { id } = location.state ;  // Desestrutura a imagem
+    const location = useLocation();
+    const { id } = location.state ;  
     
     return (
         <div>
-            {/* Passa a imagem como uma prop para a Navbar */}
             <Navbar id={id} />
         </div>
     );
